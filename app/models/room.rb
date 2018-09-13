77fddee6605daa7180b6249e23f2551137e82216
maskyo <kyohei.masuda@icloud.com>
2018-09-13 18:30:44 +0900

    set rooms callback

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,12 @@
 class Room < ApplicationRecord
   has_many :room_users
   has_many :users, through: :room_users
+
+  before_save :set_room_number
+
+  validates :room_number, uniqueness: true
+
+  def set_room_number
+    self.room_number = SecureRandom.uuid
+  end
 end
