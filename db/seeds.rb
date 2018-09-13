68b2d722a48deac0225ed391fd91218f8ab41818
maskyo <kyohei.masuda@icloud.com>
2018-09-13 18:50:46 +0900

    set seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
-sample = User.create! email: 'sample@uridoki.net', password: 'sample'
-sample1 = User.create! email: 'sample1@uridoki.net', password: 'sample1'
-sample2 = User.create! email: 'sample2@uridoki.net', password: 'sample2'
-sample3 = User.create! email: 'sample3@uridoki.net', password: 'sample3'
+user1 = User.create! email: 'sample1@uridoki.net', password: 'sample1'
+user2 = User.create! email: 'sample2@uridoki.net', password: 'sample2'
+user3 = User.create! email: 'sample3@uridoki.net', password: 'sample3'
 
+room1 = Room.create! name: 'chat room1'
+room1.users << user1
+room1.users << user2
+
+room1.messages.create! content: 'これはサンプルテキストです', user: user1
+room1.messages.create! content: 'これはサンプルテキストです', user: user2
