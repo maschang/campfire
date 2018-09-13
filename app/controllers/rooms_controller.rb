4597a15009e448e23c7845882bb7f8432c7b7c53
maskyo <kyohei.masuda@icloud.com>
2018-09-13 19:54:07 +0900

    room#indexにroom#showへの動線を追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,9 +2,13 @@ class RoomsController < ApplicationController
 
   def index
     @users = User.all
+    @rooms = current_user.rooms
   end
 
   def show
-    @messages = Message.all
+    room = Room.find_by(room_number: params[:id])
+    @messages = Message.where(room: room)
+
+    #TODO current_userがroomに入室できるかチェック
   end
 end
